adc562424fe2a9ba35aabbb4c24407382c5bc192
alaabashiyi <alaa.bashiy@gmail.com>
2021-10-17 11:57:34 +0300

    added try again button when there is internet or server error

--- components/Error/Error.js
+++ components/Error/Error.js
@@ -1,16 +1,28 @@
 import React from 'react'
-import { View, Image, Dimensions } from 'react-native'
+import { View, Image, Dimensions, Text } from 'react-native'
+import Button from '../Button/Button';
 const { width } = Dimensions.get('window');
 const idkImage = 'https://miro.medium.com/max/800/0*n-TMgnZu3nGcKLYz.jpg';
-
+import { useDispatch } from 'react-redux';
+import { getDataAction } from '../../actions/mainActions';
 
 const Error = () => {
+    const dispatch = useDispatch();
+
     return (
         <View style={{ flex: 1 }}>
             <Image
                 source={{ uri: idkImage }}
                 style={{ resizeMode: 'cover', width: '100%', height: width }}
             />
+            <Text style={{
+                fontSize: 16,
+                fontWeight: 'bold',
+                paddingHorizontal: 10,
+                paddingVertical: 15,
+                textAlign: 'center'
+            }}>Something went wrong! it could be your internet connection or our server is having some issues!</Text>
+            <Button title="TRY AGAIN" onPress={() => dispatch(getDataAction({ page: 1, keyword: '' }))} />
         </View>
     )
 }
